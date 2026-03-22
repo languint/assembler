6f8fce98f9ebb39b57bc64ab8f9538e92d9fd327
languint <longuint@proton.me>
2026-03-22 13:25:16 -0500

    fix: parameter parsing for Method::return_values and RuntimeApiRoot::global_objects

--- library/assembler-schema/src/schema/common/parameter.rs
+++ library/assembler-schema/src/schema/common/parameter.rs
@@ -2,11 +2,15 @@ use crate::schema::prelude::*;
 
 #[derive(Debug, Clone, PartialEq, serde::Deserialize)]
 pub struct Parameter {
+    /// serde::default needed because [`Method::return_values`] do not specify a `name`
+    #[serde(default)]
     pub name: String,
     pub order: u64,
     pub description: String,
     #[serde(rename = "type")]
     pub ty: RuntimeType,
+    /// Absent on [`RuntimeApiRoot::global_objects`]
+    #[serde(default)]
     pub optional: bool,
 }
 
